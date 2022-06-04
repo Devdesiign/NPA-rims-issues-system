8a71eb1eda8d100598df643c908ab84f69d969e7
d3vd3511gn <devdesiign@gmail.com>
2022-06-04 12:15:21 +0100

    Added the Form Process

---
+++ form-process.php
@@ -0,0 +1,52 @@
+<?php
+
+$port = filter_input(INPUT_POST, "port", FILTER_VALIDATE_INT); //Integer
+$sen_number = $_POST["sen-number"]; //String
+$status = $_POST["status"]; //String
+$issues = $_POST["issues"]; //String
+
+if (!$issues) {
+    die("Issue is empty");
+}
+
+//Connection Details
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rimsissues_db";
+
+$conn = mysqli_connect(
+    hostname: $host,
+    username: $username,
+    password: $password,
+    database: $dbname
+);
+
+if (mysqli_connect_errno()) {
+    die("Connection Error" . mysqli_connect_error());
+}
+
+//Inserting to DB
+
+$sql = "INSERT INTO issues (port, sen_number, status, issues) VALUES (?, ?, ?, ?)"; //Quetion marks are placeholders
+
+$stmt = mysqli_stmt_init($conn);
+
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_error($conn));
+}
+
+//Binding the values to Placeholders
+mysqli_stmt_bind_param(
+    $stmt,
+    "isss",
+    $port,
+    $sen_number,
+    $status,
+    $issues
+);
+
+mysqli_stmt_execute($stmt);
+
+echo "<h2>The issue has been captured successfully!</h2>";
